244c02661adb6ff7eb97bc0ec4632d759dacacfd
Kreniker <kitserov.mike@gmail.com>
2025-11-02 18:45:38 +0300

    check function

--- kitserov.mihail/P1/main.cpp
+++ kitserov.mihail/P1/main.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+bool is_dubl(int* row, int num) {
+    for (int i = 0; i < num - 1; i++) {
+        if (row[i] == row[i + 1]) {
+            return true;
+        }
+    }
+    return false;
+}
 int main()
 {
 
